c1300b2767900f53b1ceadea541e9fdf08152096
João Victor <joaovictorpinheiro2007@gmail.com>
2025-12-10 17:41:57 -0300

    Remove unused files

--- src/.config/hypr/scripts/wallpaper.sh
+++ src/.config/hypr/scripts/wallpaper.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-
 if [ -z "$1" ]; then
-  echo "Usage: $0 <path_to_wallpaper>"
-  exit 1
+    echo "Usage: $0 <path_to_wallpaper>"
+    exit 1
 fi
 
-WALLPAPER_PATH="$1"
-hyprctl hyprpaper reload ,"$WALLPAPER_PATH"
+hyprctl hyprpaper reload ,"$1"
