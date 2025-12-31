022ceb70d6f059d06a7dd837d08ec0f6a5852010
João Victor <joaovictorpinheiro1510@gmail.com>
2025-12-31 17:45:52 -0300

    feat(nvim): disable lint for dockerfiles

--- src/.config/nvim/lua/plugins/lint.lua
+++ src/.config/nvim/lua/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
           args = { "--config", vim.fn.expand("$HOME/.markdownlint-cli2.yaml"), "--" },
         },
       },
+      linters_by_ft = {
+        dockerfile = {},
+      },
     },
   },
   {
