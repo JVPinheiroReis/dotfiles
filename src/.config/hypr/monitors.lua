bfba34230ccedbe89b5a942bf0f30e9a9c2ca880
João Victor Pinheiro Reis <joaovictorpinheiro1510@gmail.com>
2026-09-06 19:45:44 -0300

    fix ruff formating and disable inlay hints on NeoVim

--- src/.config/hypr/monitors.lua
+++ src/.config/hypr/monitors.lua
@@ -1,5 +1,5 @@
-local omarchy_gdk_scale = 2
-local omarchy_monitor_scale = "auto"
+local omarchy_gdk_scale = 1
+local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
